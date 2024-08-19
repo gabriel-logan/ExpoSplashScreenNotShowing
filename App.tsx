d3ff812f09697bacd5a47c082f96d2402070b5ef
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-08-19 00:05:51 -0300

    chore: add readm

--- App.tsx
+++ App.tsx
@@ -1,40 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import * as SplashScreen from "expo-splash-screen";
-import { useCallback, useEffect, useState } from "react";
-
-SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-	const [appIsReady, setAppIsReady] = useState(false);
-
-	useEffect(() => {
-		async function prepare() {
-			try {
-				await new Promise((resolve) => setTimeout(resolve, 1000));
-			} catch (e) {
-				// eslint-disable-next-line no-console
-				console.warn(e);
-			} finally {
-				setAppIsReady(true);
-			}
-		}
-
-		prepare();
-	}, []);
-
-	const onLayoutRootView = useCallback(async () => {
-		if (appIsReady) {
-			await SplashScreen.hideAsync();
-		}
-	}, [appIsReady]);
-
-	if (!appIsReady) {
-		return null;
-	}
-
 	return (
-		<View style={styles.container} onLayout={onLayoutRootView}>
+		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
 			<StatusBar style="auto" />
 		</View>
---
+++ README.md
@@ -0,0 +1,40 @@
+# ExpoSplashScreenNotShowing
+
+This is a minimal reproducible example of the issue I'm facing with Expo SDK 51
+
+## Steps to reproduce
+
+1. Clone this repo
+```bash
+git clone https://github.com/gabriel-logan/ExpoSplashScreenNotShowing.git
+```
+
+2. Install dependencies
+```bash
+cd ExpoSplashScreenNotShowing
+
+npm install
+```
+
+3. Bundle the app
+```bash
+eas build --platform android --profile preview
+```
+
+4. Install the apk on a device
+
+5. Open the app
+
+## Expected behavior
+
+The splash screen should be shown
+
+## Actual behavior
+
+The splash screen is not shown
+
+## Additional information
+
+Using this reproducible example - https://github.com/gabriel-logan/ExpoSplashScreenNotShowing/tree/manualset
+
+The splash screen is shown, but I have to manually set the splash screen in the app.json file
